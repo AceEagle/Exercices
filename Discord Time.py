25024ed32de37a41e8a81d59885656028d265437
Mathieu <mathieu.vigneault.19@gmail.com>
2020-07-20 08:25:22 -0400

    Stop that

--- Discord Time.py
+++ Discord Time.py
@@ -1,5 +1,5 @@
+import subprocess
 class AppsGroups:
-    import subprocess
     def DiscordTime(self):
         subprocess.Popen([r"C:\Users\math_\AppData\Local\Discord\Update.exe"])
         subprocess.Popen([r'C:\Program Files (x86)\Steam\steam.exe'])
@@ -9,4 +9,5 @@ class AppsGroups:
         subprocess.Popen([r'C:\Users\math_\AppData\Local\JetBrains\Toolbox\\apps\PyCharm-C\ch-0\\201.6668.115\\bin\pycharm64.exe'])
         subprocess.Popen([r'C:\Users\math_\AppData\Local\gitkraken\Update.exe'])
 
-mdr
+mdr = AppsGroups()
+mdr.DiscordTime()
