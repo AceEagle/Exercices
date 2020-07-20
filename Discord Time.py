6381ee264563e8859a9bee6521b7d51a2a22c0c0
Mathieu <mathieu.vigneault.19@gmail.com>
2020-07-20 08:10:58 -0400

    raccourci start debut

---
+++ Discord Time.py
@@ -0,0 +1,12 @@
+class AppsGroups:
+    import subprocess
+    def DiscordTime(self):
+        subprocess.Popen([r"C:\Users\math_\AppData\Local\Discord\Update.exe"])
+        subprocess.Popen([r'C:\Program Files (x86)\Steam\steam.exe'])
+        subprocess.Popen([r'C:\Riot Games\Riot Client\RiotClientServices.exe'])
+
+    def CodingTime(self):
+        subprocess.Popen([r'C:\Users\math_\AppData\Local\JetBrains\Toolbox\\apps\PyCharm-C\ch-0\\201.6668.115\\bin\pycharm64.exe'])
+        subprocess.Popen([r'C:\Users\math_\AppData\Local\gitkraken\Update.exe'])
+
+mdr
